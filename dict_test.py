dc6c3fe8b3b7c2f4f1dc2b87df95e28ca3283ee7
benjwu2 <benjwu2@gmail.com>
2022-10-22 13:00:20 -0400

    test commit

--- dict_test.py
+++ dict_test.py
@@ -1,7 +1,10 @@
-dict1 = {'a':1,'b':2,'c':3}
+dict1 = {'a':[7,1],'b':[8,4],'c':3}
 print(dict1)
 print(dict1.keys())
 print(dict1.values())
 
 # prints the key with the corresponding value of 2
 print(dict1['a'])
+
+if 'b' in dict1.keys():
+    print("The value of key \'b\' is {}".format(dict1['b'][0]))
